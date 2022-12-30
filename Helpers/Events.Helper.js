ab0bb872904bdf930084cba4ef6b74512aafe826
Clifford Mwale <cliffmwale97@gmail.com>
2022-12-30 08:10:19 +0200

    Refactored the event que class

--- Helpers/Events.Helper.js
+++ Helpers/Events.Helper.js
@@ -1,65 +1,58 @@
-const amqp = require('amqplib')
-const SMSController = require('../Controllers/SMS.Controller')
-module.exports = class {
-    constructor(){
-        this.sms = new SMSController()
-        this.channel = null
-    }
-    Connect = (QueName)=>{
-       return new Promise(async (resolve, reject)=>{
-            try {
-                console.log('Connecting to Cloud RabbitMQ Server...')
-                const connection = await amqp.connect(process.env.AMQP_URL)
-                const channel = await connection.createChannel()
-                await channel.assertQueue(QueName)
-                console.log('Queue connection established!\n\nListening to %s channel', QueName)
-                resolve(channel)
-            } catch (ex) {
-                console.error('Connection Failed.\nRetrying...')
-                reject(ex)
-            }
-       })
-    }
+const amqp = require('amqplib');
 
-    Publish =  (json_data, QueName)=>{
-        return new Promise(async (resolve, reject)=>{
-            try{
-                
-                this.channel = this.channel == null ? await this.Connect(QueName) : this.channel
-                if(this.channel != "error"){
-                    console.log('Publishing event to Que...')
-                    this.channel.sendToQueue(QueName, Buffer.from(JSON.stringify(json_data)))
-                    this.channel.close()
-                    resolve('Message published to '+QueName+' Queue!')
-                }else{
-                    reject('Failed to Publish Event to Queue: '+QueName)
-                } 
-            }catch(err){
-                reject(err.message)
-            }
-        })
+class RabbitMQ {
+  constructor(queueName, options = {}) {
+    this.queueName = queueName;
+    this.options = options;
+  }
+
+  async connect(url) {
+    if (!this.connection) {
+      try {
+        this.connection = await amqp.connect(url);
+        this.channel = await this.connection.createChannel();
+      } catch (error) {
+        console.error('Error connecting to RabbitMQ:', error);
+      }
     }
-    
-    Consume = async (QueName)=>{
-        
-        try{
-            this.channel = this.channel == null ? await this.Connect(QueName) : this.channel
-            console.log('Waiting for messages...')
-            this.channel.consume(QueName,data=>{
-                console.log('Job Received!')         
-                this.sms.CaptureMessage(data.content.toString(), QueName)
-                .then(()=>{
-                    console.log('De-Queueing the Server...')
-                    this.channel.ack(data)
-                    console.log("Waiting for new messages...")
-                }).catch(err=>{
-                    console.log(err)
-                    console.log('Event maintained for later processing.')
-                })
-            })
-        }catch(e){
-            console.log(e)
-            console.log('Failed to Connect To RabbitMQ Server.\nRetrying...')
-        }
+  }
+
+  async close() {
+    if (this.connection) {
+      try {
+        await this.channel.close();
+        await this.connection.close();
+        this.connection = null;
+        this.channel = null;
+      } catch (error) {
+        console.error('Error closing RabbitMQ connection:', error);
+      }
     }
+  }
+
+  async publish(message) {
+    await this.connect();
+    try {
+      await this.channel.assertQueue(this.queueName, this.options);
+      this.channel.sendToQueue(this.queueName, Buffer.from(message));
+    } catch (error) {
+      console.error('Error publishing to RabbitMQ:', error);
+    }
+    await this.close();
+  }
+
+  async consume(callback) {
+    await this.connect();
+    try {
+      await this.channel.assertQueue(this.queueName, this.options);
+      this.channel.consume(this.queueName, (message) => {
+        callback(message.content.toString());
+        this.channel.ack(message);
+      });
+    } catch (error) {
+      console.error('Error consuming from RabbitMQ:', error);
+    }
+  }
 }
+
+module.exports = RabbitMQ;
